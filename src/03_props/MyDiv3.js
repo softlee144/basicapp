e7eb9810102c1205e750ee0c403fb435b6f6e5e2
softlee144 <ockjungbong@gmail.com>
2025-06-17 13:59:53 +0900

    props 전달2 - 구조 분해 할당 적용 (props -> {d1,d2,d3}

--- src/03_props/MyDiv3.js
+++ src/03_props/MyDiv3.js
@@ -1,11 +1,11 @@
-export default function MyDiv3(props) {
+export default function MyDiv3({ d1, d2, d3 }) {
   return (
     <div
       className="flex flex-col p-5 m-10
                     w-3/4 h-3/4
                     bg-lime-500 text-white"
     >
-      {`${props.d1} > ${props.d2} > ${props.d3}`}
+      {`${d1} > ${d2} > ${d3}`}
     </div>
   );
 }
