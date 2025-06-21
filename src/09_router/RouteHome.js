4c67d0e3e682ef4c34e4f6beabdc1af3b83ed856
softlee144 <ockjungbong@gmail.com>
2025-06-21 10:42:17 +0900

    리액트 라우터 실습2 - 변수에 태그 담아서 표시

--- src/09_router/RouteHome.js
+++ src/09_router/RouteHome.js
@@ -21,6 +21,28 @@ export default function RouteHome() {
           </ul>
         </div>
       </div>
+
+      <div className="w-1/2 grid grid-cols-2 m-10">
+        <div className="w-full flex flex-col justify-start items-center text-xl m-2 p-2">
+          <h2>Page2 값전달</h2>
+          <ul className="list-disc pl-5">
+            {/* <li>
+              <Link to={"/p2?item=사과"}>사과</Link>
+            </li>
+            <li>
+              <Link to={"/p2?item=바나나"}>바나나</Link>
+            </li>
+            <li>
+              <Link to={"/p2?item=당근"}>당근</Link>
+            </li> */}
+            <li>
+              <Link to={"/p2?item1=사과&item2=바나나&item3=당근"}>
+                사과, 바나나, 당근
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- src/09_router/pages/RoutePage2.js
+++ src/09_router/pages/RoutePage2.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
 
 export default function RoutePage2() {
-  return <div>RoutePage2</div>;
+  const fruits = ["사과", "바나나", "수박"];
+
+  const loc = useLocation();
+  //   console.log("useLocation pathname : ", loc.pathname);
+  //   console.log("useLocation search : ", loc.search);
+
+  const [sParams] = useSearchParams();
+  //   console.log("useSearchParams : ", sParams);
+
+  const qlist = [...sParams];
+  console.log(qlist);
+  const list = qlist.map((item) => (
+    <li key={item[0]}>
+      {item[1]} : {fruits.includes(item[1]) ? "과일" : "과일아님"}
+    </li>
+  ));
+
+  return (
+    <div className="w-full flex flex-col justify-center items-center">
+      <h1 className="text-2xl font-bold">RoutePage2</h1>
+      <div className="w-full flex flex-col justify-start items-center text-xl m-2 p-2">
+        <ul>{list}</ul>
+      </div>
+    </div>
+  );
 }
